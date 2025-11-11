8158b6b9e1d22b333a9d152b9ad9fa060e08caca
huydt747 <103411739+huydt747@users.noreply.github.com>
2025-11-11 08:43:57 +0700

    a

--- frontend/src/pages/Main.page.tsx
+++ frontend/src/pages/Main.page.tsx
@@ -1,9 +1,13 @@
 import { PostBlock } from "@/components/postblock";
+import { CommentBlock } from "@/components/commentblock";
+import { ReplyBlock } from "@/components/replyblock";
 
 export const MainPage: React.FC = () => {
 	return (
 		<div className='main max-w-7xl mx-auto'>
 			<PostBlock/>
+			<CommentBlock/>
+			<ReplyBlock/>
 		</div>
 	);
 };
