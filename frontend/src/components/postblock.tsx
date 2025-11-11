07da535546d46f62d642009b8717ddc2b698f2b1
huydt747 <103411739+huydt747@users.noreply.github.com>
2025-11-11 07:34:09 +0700

    post block

---
+++ frontend/src/components/postblock.tsx
@@ -0,0 +1,22 @@
+export const PostBlock: React.FC = () => {
+	return (
+		<div className="border-[#c00091] border-2 rounded p-4 m-4">
+			<div className="flex justify-between items-center mb-2">
+				<div className="flex space-x-4">
+					<div className="name">{'>>'}Tên</div>
+					<div className="date">Ngày</div>
+				</div>
+				<div className="topic px-2 py-1 bg-[#c00091] rounded">
+					Talk
+				</div>
+			</div>
+
+			<div className="content mb-2">ok</div>
+
+			<div className="interact flex space-x-4">
+				<div className="comment-count">Comment</div>
+				<div className="like-count">Like</div>
+			</div>
+		</div>
+	);
+};
--- frontend/src/pages/Main.page.tsx
+++ frontend/src/pages/Main.page.tsx
@@ -1,7 +1,9 @@
+import { PostBlock } from "@/components/postblock";
+
 export const MainPage: React.FC = () => {
 	return (
-		<div className='main text-red-500'>
-			web vip, mẫu template để tạo trang
+		<div className='main max-w-7xl mx-auto'>
+			<PostBlock/>
 		</div>
 	);
 };
